8615ee94561dee772f02d485b3603b89e2a063fd
rj <rjgilo2799@gmail.com>
2023-10-11 19:22:56 -0400

    styling to welcome modal

--- components/modals/welcome-modal.tsx
+++ components/modals/welcome-modal.tsx
@@ -2,36 +2,42 @@
 import Image from 'next/image'
 import { Dialog, DialogContent } from '../ui/dialog'
 import { welcomeModal } from '@/hooks/use-modal'
+import { Separator } from '../ui/separator'
+import { Button } from '../ui/button'
+import { useRouter } from 'next/navigation'
 
 const WelcomeModal = () => {
 
 
 
   const {isOpen, onClose} = welcomeModal()
+  const router = useRouter()
 
 
 
   return (
   
-        <Dialog open={isOpen} onOpenChange={onClose}>
-          <DialogContent className='bg-green-300/75'>
-          <Image src="/welcome-modal.jpg" className="absolute left-0 h-[270px]" width={130} height={130} alt="welcome-modal"/>
-            <div className='flex items-center justify-between m-[75px] gap-x-2'>
-              <div className='ml-10'>
-                <h2 className='font-bold'>Welcome to NutriPocket</h2>
-                <span className='text-xs text-muted-foreground'>
-                  A simple nutrition app
-                </span>
-              </div>
-              <div className=''>
-                <div>
-                  <h1>Login</h1>
-                  <h1>Continue</h1>
-                </div>
-              </div>
-            </div>
-          </DialogContent>
-        </Dialog>
+    <Dialog open={isOpen} onOpenChange={onClose}>
+    <DialogContent className='bg-green-300/75 '>
+    <Image src="/welcome-modal.jpg" className="absolute left-0 h-[302px]" width={130} height={130} alt="welcome-modal"/>
+      <div className='flex items-center justify-between m-[75px] gap-x-2'>
+        <div className='ml-10'>
+          <h2 className='font-bold'>Welcome to NutriPocket</h2>
+          <span className='text-[10px] text-muted-foreground'>
+            A simple nutrition app
+          </span>
+        </div>
+        <Separator orientation='vertical' className='bg-white'/>
+        <div>
+          <div className='flex flex-col items-center justify-between'>
+            <Button className='bg-green-500/75' onClick={() => {onClose(); router.push("/sign-in")}}>Login</Button>
+            <span>or</span>
+            <Button className='bg-green-500/75' onClick={onClose}>Continue</Button>
+          </div>
+        </div>
+      </div>
+    </DialogContent>
+  </Dialog>
     
   )
 }
